3d82a7657ec03f55c8f548b212f14cfbc6b1bc4d
ashishyadav93787asusvivo-ship-it <ashishyadav93787asusvivo@gmail.com>
2026-03-14 01:02:29 +0530

    Add files via upload

--- my_web_app.py
+++ my_web_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import google.generativeai
 
 # Apni key yahan daalein
-API_KEY = "AIzaSyAf60yEbSbKg-DHtu0U-FRa2mlVFs4uOvw"
+API_KEY = "AIzaSyBOkpcFnSIuykgFlg42NdDxpXd24O357k4"
 genai.configure(api_key=API_KEY)
 
 # Data load karein
