67ebdf74310ae3a7eabc4a9c89a75a6212cf3698
ashishyadav93787asusvivo-ship-it <ashishyadav93787asusvivo@gmail.com>
2026-03-14 21:01:08 +0530

    Update my_web_app.py

--- my_web_app.py
+++ my_web_app.py
@@ -9,7 +9,7 @@ API_KEY = "AIzaSyBV18ksORYbHlE2uUp3VOPi8N8Jr7IXL7A"
 
 try:
     genai.configure(api_key=API_KEY)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model =genai.GenerativeModel('gemini-pro')
     
     query = st.text_input("Apni bhasha mein puchiye:")
     if query:
